bae6cd1bbe50e0cf36fb6d72f6dd0c533c04c9cb
mushfiqulIslam <nnahid878@gmail.com>
2025-10-06 00:55:46 +0300

    converted entire api to singelition to inject user auth

--- src/main/java/com/studystart/jyu/config/ApplicationConfig.java
+++ src/main/java/com/studystart/jyu/config/ApplicationConfig.java
@@ -1,29 +1,36 @@
 package com.studystart.jyu.config;
 
 import com.studystart.jyu.security.AuthenticationFilter;
+import com.studystart.jyu.services.UserService;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
+import javax.inject.Singleton;
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/api")
 public class ApplicationConfig extends ResourceConfig {
-    
     public ApplicationConfig() {
-        // Register resource packages
-        packages("com.studystart.jyu.resources");
+        // scan your resources/providers OR list packages explicitly
+        packages("com.studystart.jyu.resources", "com.studystart.jyu.security");
         
-        // Register JSON Binding feature
-        register(org.glassfish.jersey.jsonb.JsonBindingFeature.class);
-
         // RBAC-annotations (@RolesAllowed/@PermitAll/@DenyAll)
         register(RolesAllowedDynamicFeature.class);
 
         register(AuthenticationFilter.class);
 
+        // HK2 binding for UserService (singleton)
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(UserService.class).to(UserService.class).in(Singleton.class);
+                // or: bindAsContract(UserService.class).in(Singleton.class);
+            }
+        });
+
         System.out.println("StudyStart JYU REST API initialized (path=/api)");
         System.out.println("RolesAllowedDynamicFeature + AuthenticationFilter registered");
-        
-        //System.out.println("StudyStart JYU REST API initialized");
     }
 }
